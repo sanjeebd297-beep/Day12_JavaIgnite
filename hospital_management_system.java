80790978ff0fe2bd44d9d5d4721c8e7f48b68cff
rite2harini <rite2harini@gmail.com>
2026-06-08 12:46:50 +0530

    Create hospital_management_system.java

---
+++ hospital_management_system.java
@@ -0,0 +1,25 @@
+/*
+Hospital Employee Management System
+
+Create the following hierarchy:
+
+Employee
+│
+| -Doctor
+│
+| -Nurse
+Requirements:
+Employee should contain:
+employeeId
+name
+Doctor should contain:
+specialization
+Nurse should contain:
+shift
+
+Polymorphism:
+
+Override:
+displayRole() for Doctor and Nurse.
+
+*/
